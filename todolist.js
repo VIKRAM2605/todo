e0e8fd3c6fe042ca418fdc3731ed372387057520
VIKRAM2605 <vikrams.al24@bitsathy.ac.in>
2024-10-24 11:58:10 +0530

    second changes

--- todolist.js
+++ todolist.js
@@ -2,22 +2,33 @@ function add(){
     var y=document.createElement("ul")
     var x=document.getElementById("iv").value
     y.append(x)
+    y.className="a"
+    y.setAttribute("onclick","ch(this)")
+  
     z=document.getElementById("ele")
     z.appendChild(y)
-    var d=document.createElement("span")
+    document.getElementById("iv").value=""
+    var d=document.createElement("button")
     var dv=document.createTextNode("Del")
     d.appendChild(dv)
     document.getElementById("ele").appendChild(d)
     d.addEventListener('click', () => {
-        z.removeChild(y);
-        z.removeChild(d);
-      });
-      z.addEventListener('click', () => {
-        z.style.textDecoration = 'line-through'
-      });
-      
+      z.removeChild(y);
+      z.removeChild(d);
+    });
+    
+}
+var faith = true;
+
+function ch(element){
+  if(faith){
+    element.style.textDecoration = 'line-through'
+    faith = false;
+  } else {
+    element.style.textDecoration = 'none'
+    faith = true;
+  }
 }
 
-function clo(){
 
-}
+
